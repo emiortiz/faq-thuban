eed1cbf1770db0c03412160bb3c1b17a220c916d
tomasruffa <45461281+tomasruffa@users.noreply.github.com>
2018-12-04 18:40:38 -0300

    Update 4-12
    Update

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// var modalAbierto
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// popup = function (modal1) {
+// modalAbierto = document.getElementById(modal1);
+// modalAbierto.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//     modalAbierto.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == modalAbierto) {
+//         modalAbierto.style.display = "none";
+//     }
+// }
